b253631c01440cabb0348d5e54692e96ecad7c8b
Violen58 <violen@narod.ru>
2023-03-28 21:45:48 +0300

    Правила Checkstyle [#504981 #403999]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,13 +2,12 @@ package ru.job4j.checkstyle;
 
 public class Broken {
 
+    private int sizeOfEmpty = 10;
     public static final String NEW_VALUE = "";
-
     public String surname;
     public String name;
-    private int sizeOfEmpty = 10;
 
-    public void broken() { }
+    public Broken() { }
 
     public void echo() { }
 

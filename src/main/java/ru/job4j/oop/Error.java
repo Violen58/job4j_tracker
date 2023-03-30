3750a797aaf4441c7a41f0fb63a525b6f46ecca5
Violen58 <violen@narod.ru>
2023-03-30 18:02:50 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        active = true;
+        status = 1;
+        message = "File not found";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printErr() {
+        System.out.println("Error: " + active);
+        System.out.println("Error code: " + status);
+        System.out.println("Description: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error eventOne = new Error();
+        eventOne.printErr();
+        Error eventZero = new Error(false, 0, "No error");
+        eventZero.printErr();
+        Error eventTwo = new Error(true, 2, "Second error");
+        eventTwo.printErr();
+    }
+}
